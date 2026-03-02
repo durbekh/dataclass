0a0427cbf2e677e5c757859a655ee091a4f47cf0
Durbek Hayotbekov <d83217311@gmail.com>
2026-03-02 16:03:42 +0500

    bu yerda dataclass mavzusida masala mavjud

---
+++ dataclass.py
@@ -0,0 +1,30 @@
+# zamonaviy
+# from dataclasses import dataclass
+# @dataclass
+# class Mashina:
+#     rang : str
+#     model : str
+#     tezlik :float
+#
+# @dataclass
+# class Avtobus(Mashina):
+#     o_soni : int
+#
+# gentra = Mashina("Qora" , "Gentra 1.8" , 200)
+# isuzu = Avtobus("Oq" , "Isuzu" , 300 , 50)
+#
+# print(isuzu.o_soni)
+# eskisi
+# class Mashina:
+#     def __init__(self, rang , model , tezlik):
+#         self.rang = rang
+#         self.__model = model
+#         self.__tezlik = tezlik
+#     def tormozla(self,qiymat):
+#         if self.__tezlik < 0:
+#             return 0
+#         return self.__tezlik - qiymat
+# gentra = Mashina("Oq","Jentra 1.6" , 100)
+#
+# print(gentra.tormozla(100))
+
