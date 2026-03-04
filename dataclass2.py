e116115af252cd771ddd5ab2186d3a8058c0a541
Durbek Hayotbekov <d83217311@gmail.com>
2026-03-04 15:39:34 +0500

    bu yerda dataclass mavzusida 15 ta masala bor

---
+++ dataclass2.py
@@ -0,0 +1,134 @@
+from dataclasses import dataclass
+# # 1
+# @dataclass
+# class Tortburchak:
+#     eni : float
+#     boyi : float
+# def yuza(eni, boyi):
+#     return eni * boyi
+# print(f"To'rtburchak yuzi { yuza(100, 100)}")
+# # 2
+# @dataclass
+# class Doira:
+#     radius : float
+# def yuza(radius):
+#     return 3.14 * (radius ** 2)
+# print(f"Doira yuzi :{yuza(4)}")
+# # 3
+# @dataclass
+# class Talaba:
+#     ism : str
+#     baho : float
+# def baho_k(ism , baho):
+#     return f"{ism } : {baho:.2f}"
+# print(baho_k("Ali" , 53))
+# # 4
+# @dataclass
+# class Tortburchak:
+#     eni : float
+#     boyi : float
+# def perimetr(eni, boyi):
+#     return 2 * (eni + boyi)
+# print(f"To'rtburchak perimetri { perimetr(100, 100)}")
+# # 5
+# @dataclass
+# class Doira:
+#     radius : float
+# def uzunligi(radius):
+#     return 3.14 * radius * 2
+# print(f"Doira uzunligi :{uzunligi(4)}")
+# # 6
+# @dataclass
+# class Mashina:
+#     model : str
+#     narxi : float
+# def qaytar(narxi):
+#     return f"Narxi :{narxi}"
+# print(qaytar(500))
+# # 7
+# @dataclass
+# class Kitob:
+#     nom : str
+#     sahifa : int
+# def sahifa_qaytar(sahifa):
+#     return f"Sahifala soni : ({sahifa}"
+# print(sahifa_qaytar(5))
+# # 8
+# @dataclass
+# class Tortburchak:
+#     eni : float
+#     boyi : float
+# def diaganal(eni, boyi):
+#     return (eni**2 + boyi**2) ** 0.5
+# print(f"Dioganal uzunligi {diaganal(6,8)}")
+# # 9
+# @dataclass
+# class Talaba:
+#     ism : str
+#     yosh : int
+# def yosh_qaytar(yosh):
+#     return yosh
+# print(f"Yosh : {yosh_qaytar(5)}")
+# # 10
+# @dataclass
+# class Uchburchak:
+#     asos : float
+#     balandlik : float
+# def yuza_hisobla(asos,balandlik):
+#     return (asos * balandlik) /2
+# print(f"Yuzi : {yuza_hisobla(5,6)}")
+# # 11
+# @dataclass
+# class Malumot:
+#     ism : str
+#     email : str
+# def malumot_chiqar(ism , email):
+#     return f"Ismi : {ism} , Email : {email}"
+# print(malumot_chiqar("Kim" ,"birornima@gmail.com" ))
+# # 12
+# @dataclass
+# class BankHisob:
+#     balance: float
+# def hisob_qaytar(balance):
+#     return f"Sizning balansimgiz {balance:.2f}"
+# print(hisob_qaytar(100))
+# # 13
+# @dataclass
+# class Tortburchak:
+#     eni : float
+#     boyi : float
+# def eni_ozgartir(self,yangieni):
+#     return self.eni == yangieni
+# def yuzi(yangieni , boyi):
+#     return f"Yuzi : {yangieni * boyi}"
+# print(yuzi(10,15))
+# # 14
+# @dataclass
+# class Talaba:
+#     ism: str
+#     bahosi: float
+# def baho_ozgartir(self, yangi_baho):
+#     return self.bahosi == yangi_baho
+# def baho_qaytar(yangi_baho):
+#     return f"Baxo : {yangi_baho}"
+# print(baho_qaytar(25))
+# # 15
+# @dataclass
+# class Doira:
+#     radius : float
+# def radius_ozgartir(self,yangi_radius ):
+#     return self.radius == yangi_radius
+# def yuza_hisobla(yangi_radius ):
+#     return f"Yuzi : {(yangi_radius ** 2) *3.14 }"
+# print(yuza_hisobla(3))
+
+
+
+
+
+
+
+
+
+
+
